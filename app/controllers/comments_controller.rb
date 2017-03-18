08d6fa1d495d8608f5d8e2162adf3954da47e29b
Tomoki Shimada <tomoki.shimada0927@gmail.com>
2017-03-18 23:23:07 +0900

    anyway commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
 
-  before_action :set_my_thread, only: [:create, :edit, :update]
   before_action :set_comment, only: [:destroy, :edit, :update]
 
   def destroy
@@ -10,6 +9,7 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @my_thread = MyThread.find(params[:my_thread_id])
     # @comment = @my_thread.comments.new(comment_params)
     # if @comment.save 今回はエラーでもリダイレクト先が一緒
     @comment = @my_thread.comments.create(comment_params)
@@ -17,12 +17,12 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    # binding.pry
   end
 
   def update
-    # やりたいけどmy_thread_idをフォームから取ってこれない。
     if @comment.update(comment_params)
-      redirect_to my_thread_path(@my_thread.id)
+      redirect_to my_thread_path(@comment.my_thread_id)
     else
       render 'edit'
     end
@@ -30,11 +30,7 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params[:comment].permit(:content)
-    end
-
-    def set_my_thread
-      @my_thread = MyThread.find(params[:my_thread_id])
+      params[:comment].permit(:body)
     end
 
     def set_comment
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :my_threads do
-    resources :comments, only: [:create, :edit, :update, :destroy]
+    resources :comments, only: [:create]
   end
+  resources :comments, only: [:edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
